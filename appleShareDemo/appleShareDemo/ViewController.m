e291b00100bb5a6938c73db57747eaaf49e33f7c
wenjiehe <123610105@qq.com>
2021-03-13 21:42:38 +0800

    ..

--- appleShareDemo/appleShareDemo/ViewController.m
+++ appleShareDemo/appleShareDemo/ViewController.m
@@ -7,6 +7,21 @@
 
 #import "ViewController.h"
 #import "WJCustomActivity.h"
+#import <Social/Social.h>
+
+
+typedef NS_ENUM(NSUInteger, ServiceShareType) {
+    ServiceShareTypeWeChat, //微信
+    ServiceShareTypeQQ, //腾讯QQ
+    ServiceShareTypeSinaWeibo,  //新浪微博
+    ServiceShareTypeTencentWeibo,  //腾讯微博
+    ServiceShareTypeFacebook,
+    ServiceShareTypeVimeo,
+    ServiceShareTypeTwitter,  //推特
+    ServiceShareTypeYouDao,  //有道笔记
+    ServiceShareTypeYiXin,  //易信
+};
+
 
 @interface ViewController ()
 
@@ -19,14 +34,19 @@
     // Do any additional setup after loading the view.
     
     
-    
 
 }
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
 {
+    //系统自带的分享
 //    [self callShare];
-    [self callCustomShare];
+    
+    //重写系统自带的分享或动作
+//    [self callCustomShareOrAction];
+    
+    //自定义分享
+    [self callCustomViewShare];
 }
 
 - (void)callShare
@@ -58,7 +78,8 @@
     
 }
 
-- (void)callCustomShare
+//自定义分享的平台或动作
+- (void)callCustomShareOrAction
 {
     UIImage *image = [UIImage imageNamed:@"wuzhuo.jpg"];
     NSURL *url = [NSURL URLWithString:@"https://www.baidu.com"];
@@ -82,6 +103,71 @@
         }
     };
     activityVC.completionWithItemsHandler = itemsBlock;
+}
+
+- (void)callCustomViewShare
+{
+//    NSString *type = [self serviceTypeWithType:ServiceShareTypeFacebook];
+    NSString *type = @"com.apple.Maps.GeneralMapsWidget";
+    if (![SLComposeViewController isAvailableForServiceType:type]) {
+        NSLog(@"没有相关配置");
+        return;
+    }
+    
+    SLComposeViewController *composeVC = [SLComposeViewController composeViewControllerForServiceType:type];
+    if(composeVC){
+//        [composeVC addImage:[UIImage imageNamed:@"wuzhuo.jpg"]];
+        [composeVC addURL:[NSURL URLWithString:@"https://www.baidu.com"]];
+        [composeVC setInitialText:@"五竹"];
+        
+        [self presentViewController:composeVC animated:YES completion:^{
+                    
+        }];
+        
+        composeVC.completionHandler = ^(SLComposeViewControllerResult result) {
+            if(result == SLComposeViewControllerResultDone){ //确定
+                NSLog(@"SLComposeViewControllerResultDone");
+            }else if (result == SLComposeViewControllerResultCancelled){ //取消
+                NSLog(@"SLComposeViewControllerResultCancelled");
+            }
+        };
+    }
+    
+}
+
+- (NSString *)serviceTypeWithType:(ServiceShareType)shareType
+{
+    NSString *type = @"";
+    switch(shareType){
+        case ServiceShareTypeWeChat:
+            type = @"com.tencent.xin.sharetimeline";
+            break;
+        case ServiceShareTypeVimeo:
+            type = @"com.apple.share.Vimeo.post";
+            break;
+        case ServiceShareTypeQQ:
+            type = @"com.tencent.mqq.ShareExtension";
+            break;
+        case ServiceShareTypeTwitter:
+            type = @"com.apple.share.Twitter.post";
+            break;
+        case ServiceShareTypeFacebook:
+            type = @"com.apple.share.Facebook.post";
+            break;
+        case ServiceShareTypeYiXin:
+            type = @"com.yixin.yixin.YXShareExtension";
+            break;
+        case ServiceShareTypeYouDao:
+            type = @"com.youdao.note.iphone.shareExtension";
+            break;
+        case ServiceShareTypeSinaWeibo:
+            type = @"com.apple.share.SinaWeibo.post";
+            break;
+        case ServiceShareTypeTencentWeibo:
+            type = @"com.apple.share.TencentWeibo.post";
+            break;
+    }
+    return type;
 }
 
 @end
