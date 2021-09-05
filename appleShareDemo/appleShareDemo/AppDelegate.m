909894324d58e01c148818155305e2ef9ee22874
hewenjie <hewj@dingchi.com>
2021-09-05 19:06:58 +0800

    增加其他app向自身app传输文件功能

--- appleShareDemo/appleShareDemo/AppDelegate.m
+++ appleShareDemo/appleShareDemo/AppDelegate.m
@@ -41,5 +41,43 @@
     // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
 }
 
+- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options
+{
+    if (self.window) {
+        if (url) {
+            NSFileManager *fileManager = [NSFileManager defaultManager];
+            NSString *fileNameStr = [NSString stringWithFormat:@"appleShareFile/%@", [url lastPathComponent]];
+            NSString *documentPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
+            NSString *filePath = [documentPath stringByAppendingPathComponent:@"shareFile"];
+            if (![fileManager fileExistsAtPath:filePath]) {
+                [fileManager createDirectoryAtPath:filePath withIntermediateDirectories:YES attributes:nil error:nil];
+            }else{
+                NSError *error = nil;
+                NSArray *fileList = [fileManager contentsOfDirectoryAtPath:filePath error:&error];
+                if (!error) {
+                    //清除之前存储的文件
+                    BOOL isDir = NO;
+                    for (NSString *file in fileList) {
+                        NSString *path = [filePath stringByAppendingPathComponent:file];
+                        [fileManager fileExistsAtPath:path isDirectory:(&isDir)];
+                        if (!isDir) {
+                            NSError *removeError = nil;
+                            [fileManager removeItemAtPath:path error:&removeError];
+                            if (removeError) {
+                                NSLog(@"清除失败:%@, 失败原因:%@", path, removeError.description);
+                            }
+                        }
+                        isDir = NO;
+                    }
+                }
+            }
+            NSData *data = [NSData dataWithContentsOfURL:url];
+            BOOL success = [data writeToFile:[NSString stringWithFormat:@"%@/%@", filePath, [url lastPathComponent]] atomically:YES];
+            NSLog(@"文件%@%@存到本地文件夹内", fileNameStr, success ? @"已经" : @"没有");
+        }
+    }
+    return YES;
+}
+
 
 @end
